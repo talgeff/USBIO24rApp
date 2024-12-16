f340c128d83d70214866a300db5b08690c028d9b
Tal Geffen <talgeff@gmail.com>
2024-12-16 17:11:20 +0200

    add class for communicate board

---
+++ USBIO24rApp/USBIO24R_Board/USBIO24_Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USBIO24rApp.USBIO24R_Board
+{
+    public static class USBIO24_Board
+    {
+        public static string BoardName { get; set; }
+    }
+}
